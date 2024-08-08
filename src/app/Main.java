8255d36fd4b0f4a1bd2f1bc1b0a59fd13b895db2
EbenBadger <ebenbadger15@gmail.com>
2024-08-08 16:29:27 +0100

    Instantiated graph object in entry point(main.java)
    To add already defined locations.

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,33 +1,51 @@
 package app;
 
 public class Main {
-//    public static Map graph = new Map();
+    public static Map graph = new Map();
 
     public static void main(String[] args) {
 
         Locations mainGate = new Locations("Main Gate");
         Locations UGFireService = new Locations("UG Fire Service");
-        Locations JonesQuarteyBuilding = new Locations("UG Fire Service");
-        Locations newBlock = new Locations("UG Fire Service");
-        Locations ugcs = new Locations("UG Fire Service");
-        Locations balmeLibrary = new Locations("UG Fire Service");
-        Locations newNBlock = new Locations("UG Fire Service");
-        Locations CSDepartment = new Locations("UG Fire Service");
-        Locations lawSchool = new Locations("UG Fire Service");
-        Locations businessSchool = new Locations("UG Fire Service");
-        Locations greatHall = new Locations("UG Fire Service");
-        Locations pentHall = new Locations("UG Fire Service");
-        Locations voltaHall = new Locations("UG Fire Service");
-        Locations akuafoHall = new Locations("UG Fire Service");
-        Locations legonHall = new Locations("UG Fire Service");
-        Locations commonWealthHall = new Locations("UG Fire Service");
-        Locations athleticOval = new Locations("UG Fire Service");
-        Locations diaspora = new Locations("UG Fire Service");
-        Locations nightMarket = new Locations("UG Fire Service");
+        Locations JonesQuarteyBuilding = new Locations("Jones Quartey Building(JQB)");
+        Locations newBlock = new Locations("N Block");
+        Locations ugcs = new Locations("UGCS");
+        Locations balmeLibrary = new Locations("Balme Library");
+        Locations newNBlock = new Locations("NNB");
+        Locations CSDepartment = new Locations("Department of Computer Science");
+        Locations lawSchool = new Locations("School of Law");
+        Locations businessSchool = new Locations("UGBS");
+        Locations greatHall = new Locations("Great Hall");
+        Locations pentHall = new Locations("Pent Hall");
+        Locations voltaHall = new Locations("Volta Hall");
+        Locations akuafoHall = new Locations("Akuafo Hall");
+        Locations legonHall = new Locations("Legon Hall");
+        Locations commonWealthHall = new Locations("Commonwealth Hall");
+        Locations athleticOval = new Locations("Athletic Oval");
+        Locations diaspora = new Locations("Diaspora Halls(Limann, Kwapong, etc.)");
+        Locations nightMarket = new Locations("Night Market");
 
 
         System.out.println("Is this working?");
 
-//        graph.addVertex(mainGate);
+        graph.addVertex(mainGate);
+        graph.addVertex(UGFireService);
+        graph.addVertex(JonesQuarteyBuilding);
+        graph.addVertex(newBlock);
+        graph.addVertex(ugcs);
+        graph.addVertex(balmeLibrary);
+        graph.addVertex(newNBlock);
+        graph.addVertex(CSDepartment);
+        graph.addVertex(lawSchool);
+        graph.addVertex(businessSchool);
+        graph.addVertex(greatHall);
+        graph.addVertex(pentHall);
+        graph.addVertex(voltaHall);
+        graph.addVertex(akuafoHall);
+        graph.addVertex(legonHall);
+        graph.addVertex(commonWealthHall);
+        graph.addVertex(athleticOval);
+        graph.addVertex(diaspora);
+        graph.addVertex(nightMarket);
     }
 }
