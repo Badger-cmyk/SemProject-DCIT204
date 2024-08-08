71de179d706b0083d233ae5d41d4b639d8997f35
EbenBadger <ebenbadger15@gmail.com>
2024-08-08 13:17:37 +0100

    Added a Locations class
    To instantiate new locations in the Main.java file

---
+++ src/app/Main.java
@@ -0,0 +1,13 @@
+package app;
+
+public class Main {
+//    public static Map graph = new Map();
+
+    public static void main(String[] args) {
+
+        Locations mainGate = new Locations("Main Gate");
+        System.out.println("Is this working?");
+
+//        graph.addVertex(mainGate);
+    }
+}
