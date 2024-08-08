63234c6ee66b325de4fb4b9fed69d8545e0529f9
EbenBadger <ebenbadger15@gmail.com>
2024-08-08 13:38:34 +0100

    Created location objects.
    To serve as options in the user interface later

--- src/app/Main.java
+++ src/app/Main.java
@@ -6,6 +6,26 @@ public class Main {
     public static void main(String[] args) {
 
         Locations mainGate = new Locations("Main Gate");
+        Locations UGFireService = new Locations("UG Fire Service");
+        Locations JonesQuarteyBuilding = new Locations("UG Fire Service");
+        Locations newBlock = new Locations("UG Fire Service");
+        Locations ugcs = new Locations("UG Fire Service");
+        Locations balmeLibrary = new Locations("UG Fire Service");
+        Locations newNBlock = new Locations("UG Fire Service");
+        Locations CSDepartment = new Locations("UG Fire Service");
+        Locations lawSchool = new Locations("UG Fire Service");
+        Locations businessSchool = new Locations("UG Fire Service");
+        Locations greatHall = new Locations("UG Fire Service");
+        Locations pentHall = new Locations("UG Fire Service");
+        Locations voltaHall = new Locations("UG Fire Service");
+        Locations akuafoHall = new Locations("UG Fire Service");
+        Locations legonHall = new Locations("UG Fire Service");
+        Locations commonWealthHall = new Locations("UG Fire Service");
+        Locations athleticOval = new Locations("UG Fire Service");
+        Locations diaspora = new Locations("UG Fire Service");
+        Locations nightMarket = new Locations("UG Fire Service");
+
+
         System.out.println("Is this working?");
 
 //        graph.addVertex(mainGate);
